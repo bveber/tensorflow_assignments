525969ed92bb60c8bdb0fc8c9e5a968bf6c24714
Brandon Veber <bveber@bveber-osx.local>
2016-01-28 15:21:00 -0600

    stupid indent error

--- load_notMNIST.py
+++ load_notMNIST.py
@@ -132,5 +132,5 @@ except Exception as e:
   print 'Unable to save data to', pickle_file, ':', e
   raise
 
- statinfo = os.stat(pickle_file)
+statinfo = os.stat(pickle_file)
 print 'Compressed pickle size:', statinfo.st_size
