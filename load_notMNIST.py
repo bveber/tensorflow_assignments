8c2bcd1d78731655c74e711d7995d4ca2a6d97a4
Brandon Veber <bveber@bveber-osx.local>
2016-01-28 16:34:03 -0600

    filename fix

--- load_notMNIST.py
+++ load_notMNIST.py
@@ -18,15 +18,15 @@ def maybe_download(filename, expected_bytes):
     if not os.path.exists(filename):
         filename, _ = urllib.urlretrieve(url + filename, filename)
         statinfo = os.stat(filename)
-        if statinfo.st_size == expected_bytes:
-            print 'Found and verified', filename
+        # if statinfo.st_size == expected_bytes:
+        #     print 'Found and verified', filename
     else:
         raise Exception(
                     'Failed to verify' + filename + '. Can you get to it with a browser?')
     return filename
 
-# train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
-# test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
+train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
 #
 # print 'done'
 
